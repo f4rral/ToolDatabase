7ae33cabff91008cec1c64b35b70cf95a7231c74
f4rral <f4rral@gmail.com>
2025-03-09 21:42:57 +0300

    Тест Slider и ExposedDropdownMenuBox

--- app/src/main/java/com/example/tooldatabase/screens/home/Home.kt
+++ app/src/main/java/com/example/tooldatabase/screens/home/Home.kt
@@ -1,13 +1,26 @@
 package com.example.tooldatabase.screens.home
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,6 +33,8 @@ import com.example.tooldatabase.ui.elements.ButtonText
 import com.example.tooldatabase.viewmodels.ToolBodyListVM
 import com.example.tooldatabase.viewmodels.ToolBodyListVMFactory
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun HomeScreen() {
     val vmToolBodyList: ToolBodyListVM = viewModel(
@@ -28,6 +43,11 @@ fun HomeScreen() {
 
     val items = vmToolBodyList.items.collectAsState()
     val stateFilter = vmToolBodyList.stateFilterFlow.collectAsState()
+    var sliderPosition = mutableFloatStateOf(0f)
+
+    val options = listOf(null, 8, 12, 16, 25, 32, 40, 50, 63, 80, 100, 125, 160)
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOptionText by remember { mutableStateOf("") }
 
     ScreenLayout(
         title = "Home",
@@ -38,7 +58,64 @@ fun HomeScreen() {
                 .padding(start = 8.dp, end = 8.dp)
                 .fillMaxSize()
         ) {
-            TextField(
+            Slider(
+                value = sliderPosition.floatValue,
+                valueRange = 8f..1000f,
+                onValueChange = { sliderPosition.floatValue = it }
+            )
+
+            Slider(
+                value = sliderPosition.floatValue,
+                onValueChange = { sliderPosition.floatValue = it },
+                steps = 10,
+                valueRange = 8f..1000f
+            )
+
+            Slider(
+                value = sliderPosition.floatValue,
+                onValueChange = { sliderPosition.floatValue = it },
+                steps = 10,
+                valueRange = 8f..1000f
+            )
+
+            Text(text = sliderPosition.floatValue.toString())
+
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = { expanded = !expanded },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                TextField(
+                    value = selectedOptionText,
+                    onValueChange = {},
+                    readOnly = true,
+                    modifier = Modifier.menuAnchor().fillMaxWidth(),
+                    label = { Text(text = "label") }
+                )
+
+                ExposedDropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    options.forEach { option ->
+                        DropdownMenuItem(
+                            text = { Text(text = option.toString()) },
+                            onClick = {
+                                selectedOptionText = option.toString()
+                                expanded = false
+
+                                vmToolBodyList.updateFilter(
+                                    filter = stateFilter.value.copy(nmlDiameter = option)
+                                )
+                            },
+                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                        )
+                    }
+                }
+            }
+
+            OutlinedTextField(
+                label = { Text(text = "Номинальный диаметр, мм")},
                 modifier = Modifier.fillMaxWidth(),
                 value = if (stateFilter.value.nmlDiameter != null) stateFilter.value.nmlDiameter.toString() else "",
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
@@ -95,4 +172,48 @@ fun HomeScreenPreview() {
             toolBodyList = com.example.tooldatabase.ui.components.tool_body.ToolBody.toolBodyListFake,
         )
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Select() {
+    val options = listOf("Option 1", "Option 2", "Option 3", "Option 4", "Option 5")
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOptionText by remember { mutableStateOf("") }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        TextField(
+            value = selectedOptionText,
+            onValueChange = {},
+            readOnly = true,
+            modifier = Modifier.menuAnchor().fillMaxWidth(),
+            label = { Text(text = "label") }
+        )
+
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            options.forEach { option ->
+                DropdownMenuItem(
+                    text = { Text(text = option) },
+                    onClick = {
+                        selectedOptionText = option
+                        expanded = false
+                    },
+                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SelectPreview() {
+    Select()
 }
